121126873ece3a6d11c0766909edd1dbbf6584eb
JanPf <janpf@users.noreply.github.com>
2020-01-30 23:39:48 +0100

    naja

--- b_c_analysis/plot_b_c.py
+++ b_c_analysis/plot_b_c.py
@@ -6,7 +6,6 @@ from mpl_toolkits import mplot3d
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--csv", type=str, help="path to a csv")
-parser.add_argument("--out", type=str, help="dest for edited images")
 args = parser.parse_args()
 
 data = []
