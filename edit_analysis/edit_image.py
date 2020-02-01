b32a4566be00afaef150286ff93d5614016c9108
JanPf <janpf@users.noreply.github.com>
2020-02-01 16:56:02 +0100

    wow.

--- edit_analysis/edit_image.py
+++ edit_analysis/edit_image.py
@@ -5,26 +5,26 @@ import numpy as np
 
 from skimage import img_as_float, exposure, io
 from PIL import Image
-from pillow_lut as lut
+import pillow_lut as lut
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--image", type=str, help="path to a single image")
 parser.add_argument("--imageFolder", type=str, help="path to a folder of images (if this is supplied '--image' is ignored)")
 parser.add_argument("--parameter", type=str, help="what to change: brightness, contrast...")
 parser.add_argument("--range", type=float, help="+- range to change the parameter in", default=5)
-parser.add_argument("--step", type=float, help="step size of changes", default=.2)
+parser.add_argument("--step", type=float, help="step size of changes", default=0.2)
 parser.add_argument("--out", type=str, help="dest for edited images")
 args = parser.parse_args()
 
 
-#brightness
-#exposure
-#contrast
-#warmth
-#saturation
-#vibrance
-#hue
-#l_contrast
+# brightness
+# exposure
+# contrast
+# warmth
+# saturation
+# vibrance
+# hue
+# l_contrast
 
 
 def edit_image(img_path):
@@ -34,9 +34,9 @@ def edit_image(img_path):
         io.imsave(Path(args.out) / args.parameter / f"{Path(args.image).stem}_{args.parameter}_{args.change}_.jpg", img_adapteq)
 
     else:
-        for change in np.arange(-args.range, args.range+ (args.step/2), args.step):
+        for change in np.arange(-args.range, args.range + (args.step / 2), args.step):
             img = Image.open(img_path)
-            img_filter = lut.rgb_color_enhance(16, **{args.parameter: args.change}) # does this work?
+            img_filter = lut.rgb_color_enhance(16, **{args.parameter: args.change})  # does this work?
             img.filter(img_filter).save(Path(args.out) / args.parameter / f"{Path(args.image).stem}_{args.parameter}_{args.change}_.jpg")
 
 
