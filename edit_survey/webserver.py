71d20ba4d22b9f2f2add15b794d3bd925cedb5e9
JanPf <janpf@users.noreply.github.com>
2020-02-13 18:31:05 +0100

    added local contrast to rotation

--- edit_survey/webserver.py
+++ edit_survey/webserver.py
@@ -8,6 +8,7 @@ from random import shuffle
 from typing import Dict, Tuple
 
 import cv2
+import numpy as np
 import pillow_lut as lut
 from flask import Flask, abort, redirect, render_template, request
 from flask.helpers import send_file, url_for
@@ -27,15 +28,13 @@ with open(args.imageFile, "r") as imgFile:
 poll_log = open(Path(args.out) / "poll.log", "a", buffering=1)
 
 
-def edit_and_serve_image(img_path: str, changes: Dict[str, float]):  # FIXME be able to apply lcontrast in addition to other parameter changes
+def edit_and_serve_image(img_path: str, changes: Dict[str, float]):
     if "lcontrast" in changes:
-
         img = cv2.imread(str(img_path))
         img_lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
         lab_planes = cv2.split(img_lab)
 
-        clahe = cv2.createCLAHE(clipLimit=changes["lcontrast"], tileGridSize=(8, 8))
-        cl = clahe.apply(lab_planes[0])
+        cl = cv2.createCLAHE(clipLimit=changes["lcontrast"], tileGridSize=(8, 8)).apply(lab_planes[0])
 
         limg = cv2.merge(lab_planes)
         img = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
@@ -54,19 +53,27 @@ def edit_and_serve_image(img_path: str, changes: Dict[str, float]):  # FIXME be 
 
 
 def random_parameters() -> Tuple[str, Tuple[float, float]]:
-    parameters = {"brightness": [-1, 1], "exposure": [-5, 5], "contrast": [-1, 5], "warmth": [-1, 1], "saturation": [-1, 5], "vibrance": [-1, 5], "lcontrast": [0.1, 40]}  # TODO hue[0,1] and lcontrast
-    if random.choice(["single"]) == "single":  # how many parameters to change at once
-        pos_neg = random.choice(["positiv", "negative", "interval"])  # in order to not match a positive change with a negative one
+    parameters = {"brightness": [-1, 1], "exposure": [-5, 5], "contrast": [-1, 5], "warmth": [-1, 1], "saturation": [-1, 5], "vibrance": [-1, 5], "lcontrast": [0.1, 40]}  # TODO hue[0,1]
+    if random.choice(["single"]) == "single":  # how many parameters to change at once # TODO add ability to change multiple parameters at once
         change = random.choice(list(parameters.keys()))
-        N = 1
-        if pos_neg == "positive":
-            changeVal = (0, round(random.uniform(0, parameters[change][1]), N))
-        elif pos_neg == "negative":
-            changeVal = (0, round(random.uniform(parameters[change][0], 0), N))
+        if change == "lcontrast":
+            pos_neg = random.choice(["positiv", "interval"])
+            lcontrast_vals = [round(val, 1) for val in list(np.arange(0.1, 1, 0.1)) + list(range(1, 10)) + list(range(10, 41, 5))]  # [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 40]
+            if pos_neg == "positive":
+                changeVal = (0, random.choice(lcontrast_vals))
+            else:
+                changeVal = (random.choice(lcontrast_vals), random.choice(lcontrast_vals))
         else:
-            changeVal = (round(random.uniform(parameters[change][0], parameters[change][1]), N), round(random.uniform(parameters[change][0], parameters[change][1]), N))
-            while not math.copysign(1, changeVal[0]) == math.copysign(1, changeVal[1]):  # make sure to not compare an image to another one, which has been edited in the other "direction"
-                changeVal = (changeVal[0], round(random.uniform(parameters[change][0], parameters[change][1]), N))
+            pos_neg = random.choice(["positiv", "negative", "interval"])  # in order to not match a positive change with a negative one
+            N = 1
+            if pos_neg == "positive":
+                changeVal = (0, round(random.uniform(0, parameters[change][1]), N))
+            elif pos_neg == "negative":
+                changeVal = (0, round(random.uniform(parameters[change][0], 0), N))
+            else:
+                changeVal = (round(random.uniform(parameters[change][0], parameters[change][1]), N), round(random.uniform(parameters[change][0], parameters[change][1]), N))
+                while not math.copysign(1, changeVal[0]) == math.copysign(1, changeVal[1]):  # make sure to not compare an image to another one, which has been edited in the other "direction"
+                    changeVal = (changeVal[0], round(random.uniform(parameters[change][0], parameters[change][1]), N))
     return change, changeVal
 
 
