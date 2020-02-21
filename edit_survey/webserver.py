172c2079a088e5de9e5829458be5ccc17834794f
JanPf <janpf@users.noreply.github.com>
2020-02-21 19:32:51 +0100

    details

--- edit_survey/webserver.py
+++ edit_survey/webserver.py
@@ -157,7 +157,7 @@ def log_request_info():
         return redirect(url_for("login"))
 
 
-def load_app(imgFile="/data/imgs.txt", imageFolder="/data/images", out="/data/logs"):  # for gunicorn # https://github.com/benoitc/gunicorn/issues/135
+def load_app(imgFile="/data/train.txt", imageFolder="/data/images", out="/data/logs"):  # for gunicorn # https://github.com/benoitc/gunicorn/issues/135
     logging.basicConfig(filename=Path(out) / "flask.log", level=logging.DEBUG)  # TODO separate log files
     gunicorn_error_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers.extend(gunicorn_error_logger.handlers)
