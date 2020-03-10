c5e2f1eafffe883414172353d7342534cc2c4e65
JanPf <janpf@users.noreply.github.com>
2020-03-10 20:04:59 +0100

    added darktable for temp and tint

--- edit_image.py
+++ edit_image.py
@@ -11,8 +11,6 @@ import cv2
 from jinja2 import Template
 from PIL import Image
 
-# TODO temp tint
-
 
 def edit_image(img_path: str, change: str, value: float, out_path: str = None) -> Image:  # TODO alles mal checken
 
@@ -54,12 +52,11 @@ def edit_image(img_path: str, change: str, value: float, out_path: str = None) -
             change_str = "".join(["%02x" % b for b in bytearray(pack("f", value))])
 
         if "temperature" == change or "tint" == change:
-            raise ("aaaarg")
             template_file = "./darktable_xmp/temperature.xmp"
             if "temperature" == change:
-                change_str = f"f3efbf3f0000803fa91a073f0000807f"
+                change_str = parameter_range["temperature"]["rangemapping"][change]
             elif "tint" == change:
-                change_str = f"f3efbf3f0000803fa91a073f0000807f"
+                change_str = parameter_range["tint"]["rangemapping"][change]
 
         with open(template_file) as template_file:
             Template(template_file.read()).stream(value=change_str).dump(edit_file)
@@ -102,9 +99,66 @@ parameter_range["vibrance"]["default"] = 25
 parameter_range["vibrance"]["max"] = 100
 parameter_range["vibrance"]["range"] = [0, 5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100]
 
-# parameter_range["temperature"]["min"] = 1000
-# parameter_range["temperature"]["default"] = 6500
-# parameter_range["temperature"]["max"] = 12000
+parameter_range["temperature"]["min"] = 1000
+parameter_range["temperature"]["default"] = 6500
+parameter_range["temperature"]["max"] = 25000
+parameter_range["temperature"]["range"] = [2000, 3000, 4000, 5000, 6000, 6500, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000, 16000, 17000, 18000, 19000, 20000, 21000, 22000, 23000, 24000, 25000]
+parameter_range["temperature"]["rangemapping"] = {  # encoded RGB values, since darktable can't save temperature directly
+    "2000": "6c71833e0000803f495cfd410000807f",
+    "3000": "a623f43e0000803f9b9446400000807f",
+    "4000": "e3832c3f0000803f705af13f0000807f",
+    "5000": "f954543f0000803f9317ad3f0000807f",
+    "6000": "ef39733f0000803f8c138b3f0000807f",
+    "6500": "7405803f0000803fc301803f0000807f",
+    "7000": "f39e853f0000803f72e86e3f0000807f",
+    "8000": "17208f3f0000803ffed8553f0000807f",
+    "9000": "c8c2963f0000803fdeb9443f0000807f",
+    "10000": "d3029d3f0000803fd95f383f0000807f",
+    "11000": "5035a23f0000803fcc172f3f0000807f",
+    "12000": "9e96a63f0000803fb3e4273f0000807f",
+    "13000": "ae52aa3f0000803f182a223f0000807f",
+    "14000": "328aad3f0000803f68821d3f0000807f",
+    "15000": "f555b03f0000803f08a9193f0000807f",
+    "16000": "13c9b23f0000803f176e163f0000807f",
+    "17000": "7ff2b43f0000803f37af133f0000807f",
+    "18000": "10deb63f0000803f2753113f0000807f",
+    "19000": "4295b83f0000803ff7460f3f0000807f",
+    "20000": "c61fba3f0000803f2e7c0d3f0000807f",
+    "21000": "e483bb3f0000803f93e70b3f0000807f",
+    "22000": "cbc6bc3f0000803f50800a3f0000807f",
+    "23000": "caecbd3f0000803f593f093f0000807f",
+    "24000": "7ff9be3f0000803f021f083f0000807f",
+    "25000": "f3efbf3f0000803fa91a073f0000807f",
+}
+
+parameter_range["tint"]["min"] = 0.2
+parameter_range["tint"]["default"] = 1.0
+parameter_range["tint"]["max"] = 2.3
+parameter_range["tint"]["range"] = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3]
+parameter_range["tint"]["rangemapping"] = {  # encoded RGB values, since darktable can't save tint directly
+    "0.2": "f16ad3bf0000803f79fd38420000807f",
+    "0.3": "020b05c00000803f063824410000807f",
+    "0.4": "0bee3ac00000803fb6e0af400000807f",
+    "0.5": "cd4cabc00000803f804067400000807f",
+    "0.6": "bce1b4c20000803fc2b926400000807f",
+    "0.7": "c63ea9400000803f370cfd3f0000807f",
+    "0.8": "d5ba18400000803fa425c63f0000807f",
+    "0.9": "5d93ba3f0000803fc4469e3f0000807f",
+    "1.0": "7405803f0000803fc301803f0000807f",
+    "1.1": "da553a3f0000803f797c503f0000807f",
+    "1.2": "13180c3f0000803fb52e2a3f0000807f",
+    "1.3": "6c5ad63e0000803fa5a70a3f0000807f",
+    "1.4": "5519a53e0000803fb980e03e0000807f",
+    "1.5": "6cb97d3e0000803f31a2b33e0000807f",
+    "1.6": "cea2403e0000803f8f098d3e0000807f",
+    "1.7": "dfb60e3e0000803f1af8563e0000807f",
+    "1.8": "df4eca3d0000803f41181c3e0000807f",
+    "1.9": "8a08843d0000803f810cd03d0000807f",
+    "2.0": "35ae0f3d0000803f9792663d0000807f",
+    "2.1": "f6a21d3c0000803f1089803c0000807f",
+    "2.2": "1f1f4fbc0000803f8250abbc000080ff",
+    "2.3": "4c3404bd0000803f0c825dbd000080ff",
+}
 
 parameter_range["lcontrast"]["min"] = 0  # I think minimum is impossible (possibly due to c bindings type conversions)
 parameter_range["lcontrast"]["default"] = 0
