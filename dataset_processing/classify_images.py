6868e15c2ed05ac2afb3f523f265da7ccf6fb7c8
JanPf <janpf@users.noreply.github.com>
2020-07-21 20:18:33 +0200

    added margin finder and image classifier

--- dataset_processing/classify_images.py
+++ dataset_processing/classify_images.py
@@ -1,1 +1,14 @@
-# TODO implement
+from pathlib import Path
+from imageai.Prediction import ImagePrediction
+
+orig_dir: Path = Path("/scratch/pexels/images")
+
+prediction = ImagePrediction()
+prediction.setModelTypeAsDenseNet()  # https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/DenseNet-BC-121-32.h5
+prediction.setModelPath("/tmp/DenseNet-BC-121-32.h5")
+prediction.loadModel()
+
+for img in orig_dir.iterdir():
+    predictions, probabilities = prediction.predictImage(str(img), result_count=5)
+    for eachPrediction, eachProbability in zip(predictions, probabilities):
+        print(f"{eachPrediction}:{eachProbability}")
--- model/identify_margin.py
+++ model/identify_margin.py
@@ -1,51 +1,68 @@
-import argparse
 import sys
 from pathlib import Path
 
-import numpy as np
+import math
+from PIL import Image
 import torch
 import torchvision.models as models
 import torchvision.transforms as transforms
 
 sys.path[0] = "/workspace"
 
-from model.datasets import PexelsRedis
 from model.NIAA import NIAA
+from edit_image import parameter_range
 
 
-def main(config):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # fmt: off
-    Pexels_train_transform = transforms.Compose([
-        transforms.Scale(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor()])
-    # fmt: on
+orig_dir: Path = Path("/scratch/pexels/images")
+edited_dir: Path = Path("/scratch/pexels/edited_images")
 
-    base_model = models.vgg16(pretrained=True)
-    model = NIAA(base_model)
+# fmt: off
+transform = transforms.Compose([
+    transforms.Scale(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor()])
+# fmt: on
 
-    Pexels_train = PexelsRedis(mode="train", transforms=Pexels_train_transform)
-    Pexels_train_loader = torch.utils.data.DataLoader(Pexels_train, batch_size=config.train_batch_size, shuffle=True, drop_last=True, num_workers=config.num_workers)
+base_model = models.vgg16(pretrained=True)
+model = NIAA(base_model).to(device)
 
-    for data in Pexels_train_loader:
-        img1 = data["img1"].to(device)
-        img2 = data["img2"].to(device)
+for img in orig_dir.iterdir():
+    for parameter in parameter_range.keys():
+        for lchange in parameter_range[parameter]["range"]:  # iterating over all possible changes
+            for rchange in parameter_range[parameter]["range"]:  # iterating over all possible changes
+                if math.isclose(lchange, rchange):  # don't compare 0.5 to 0.5 for example
+                    continue
+                if rchange < lchange:  # only compare 0.4 to 0.5 but not 0.5 to 0.4
+                    continue
+                if lchange < parameter_range[parameter]["default"] and rchange > parameter_range[parameter]["default"] or lchange > parameter_range[parameter]["default"] and rchange < parameter_range[parameter]["default"]:
+                    continue
 
-        with torch.no_grad():
-            out1, out2 = model(img1, img2, "siamese")
+                lRelDist = abs((parameter_range[parameter]["default"]) - (lchange))
+                rRelDist = abs((parameter_range[parameter]["default"]) - (rchange))
+                lRelDist = round(lRelDist, 2)
+                rRelDist = round(rRelDist, 2)
 
-        # TODO print out1, out2
+                if lRelDist > rRelDist or math.isclose(lRelDist, rRelDist):  # smaller change always has to be img1/imgl, as the Distance Loss assumes the "more original" image is the first
+                    continue
 
+                if math.isclose(lchange, parameter_range[parameter]["default"]):
+                    imgl = str(orig_dir / img)
+                else:
+                    imgl = str(edited_dir / parameter / str(lchange) / img)
+                if math.isclose(rchange, parameter_range[parameter]["default"]):
+                    imgr = str(orig_dir / img)
+                else:
+                    imgr = str(edited_dir / parameter / str(rchange) / img)
 
-if __name__ == "__main__":
+                img1 = transform(Image.open(imgl).convert("RGB")).unsqueeze(dim=0).to(device)
+                img2 = transform(Image.open(imgr).convert("RGB")).unsqueeze(dim=0).to(device)
 
-    parser = argparse.ArgumentParser()
+                with torch.no_grad():
+                    out1, out2 = model(img1, img2, "siamese")
 
-    # misc
-    parser.add_argument("--log_dir", type=str, default="/scratch/train_logs/pexels/cold")
+                out1 = out1.data[0]
+                out2 = out2.data[0]
 
-    config = parser.parse_args()
-
-    main(config)
+                print(f"{out1}, {out2}, {parameter}, {lchange}, {rchange}, {lRelDist}, {rRelDist}")
