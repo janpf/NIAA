aa1b348ef9c9864cbbbbf151239212027514dbd4
JanPf <janpf@users.noreply.github.com>
2020-07-22 22:25:09 +0200

    forgot requirements

--- dataset_processing/classify_images.py
+++ dataset_processing/classify_images.py
@@ -9,6 +9,9 @@ prediction.setModelPath("/tmp/DenseNet-BC-121-32.h5")
 prediction.loadModel()
 
 for img in orig_dir.iterdir():
+    print(img.name, end="")
+    print(",", end="")
     predictions, probabilities = prediction.predictImage(str(img), result_count=5)
     for eachPrediction, eachProbability in zip(predictions, probabilities):
-        print(f"{eachPrediction}:{eachProbability}")
+        print(f"{eachPrediction}:{eachProbability},", end="")
+    print(flush=True)
