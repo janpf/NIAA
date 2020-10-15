c20168fe93be6640d0264ccb0eb050ec87ca7a27
JanPf <janpf@users.noreply.github.com>
2020-10-15 23:14:19 +0200

    updating dynamic model loader

--- IA/train.py
+++ IA/train.py
@@ -11,8 +11,7 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
 sys.path[0] = "/workspace"
-from IA.dataset import SSPexelsNonTar as SSPexels
-from IA.losses import EfficientRankingLoss
+from IA.dataset import SSPexelsSmall as SSPexels
 from IA.IA import IA
 from IA.utils import mapping
 
@@ -24,30 +23,23 @@ parser.add_argument("--dense_lr", type=float, default=0.0001)
 parser.add_argument("--styles_margin", type=float, default=0.2)
 parser.add_argument("--technical_margin", type=float, default=0.2)
 parser.add_argument("--composition_margin", type=float, default=0.2)
-parser.add_argument("--base_model", type=str)
-parser.add_argument("--train_from", type=str)
 parser.add_argument("--lr_decay_rate", type=float, default=0.9)
 parser.add_argument("--train_batch_size", type=int, default=8)
 parser.add_argument("--val_batch_size", type=int, default=8)
 parser.add_argument("--num_workers", type=int, default=60)
-parser.add_argument("--epochs", type=int, default=100)
-parser.add_argument("--fix_features", action="store_true")
-
 # misc
 parser.add_argument("--log_dir", type=str, default="/scratch/train_logs/IA/pexels/")
 parser.add_argument("--ckpt_path", type=str, default="/scratch/ckpts/IA/pexels/")
-parser.add_argument("--warm_start", action="store_true")
-parser.add_argument("--warm_start_epoch", type=int, default=0)
 
 config = parser.parse_args()
 
-settings = [config.base_model, str(config.conv_base_lr)]
+settings = [config.scores]
 
-if config.fix_features:
-    settings.append("fix_features")
+if config.change_regress:
+    settings.append("change_regress")
 
-if config.softplus:
-    settings.append("softplus")
+if config.change_class:
+    settings.append("change_class")
 
 for s in settings:
     config.log_dir = str(Path(config.log_dir) / s)
@@ -61,31 +53,28 @@ margin["composition"] = config.composition_margin
 logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
 # logging.getLogger("PIL.PngImagePlugin").setLevel(logging.INFO)
 
-if not config.warm_start and Path(config.log_dir).exists():
-    raise "train script got restarted, although previous run exists"
 Path(config.log_dir).mkdir(parents=True, exist_ok=True)
 writer = SummaryWriter(log_dir=config.log_dir)
 
 device = torch.device("cuda" if cuda.is_available() else "cpu")
 
 logging.info("loading model")
-ia = IA(mapping=mapping, fix_features=config.fix_features).to(device)
+ia = IA(scores=config.scores, change_regress=config.change_regress, change_class=config.change_class, mapping=mapping, fix_features=config.fix_features).to(device)
 
 # loading checkpoints, ... or not
-if config.warm_start:
-    logging.info("loading checkpoint")
-    # Path(config.ckpt_path).mkdir(parents=True, exist_ok=True)
-    ia.load_state_dict(torch.load(str(Path(config.ckpt_path) / f"epoch-{config.warm_start_epoch}.pth")))
-    logging.info(f"Successfully loaded model epoch-{config.warm_start_epoch}.pth")
-
-elif config.train_from:
-    logging.info("loading from")
-    ia.load_state_dict(torch.load(config.train_from))
-
-else:
-    logging.info("starting cold")
-    if Path(config.ckpt_path).exists():
-        raise "model already trained, but cold training was used"
+warm_epoch = 0
+logging.info("checking for checkpoints")
+if Path(config.ckpt_path).exists():
+    logging.info("loading checkpoints")
+    if not (Path(config.ckpt_path) / f"epoch-0.pth").exists():
+        logging.info("none found")
+    else:
+        for warm_epoch in range(100):
+            p = Path(config.ckpt_path) / f"epoch-{warm_epoch}.pth"
+            if not (Path(config.ckpt_path) / f"epoch-{warm_epoch+1}.pth").exists():
+                break
+        ia.load_state_dict(torch.load(str(p)))
+        logging.info(f"Successfully loaded model {p}")
 
 logging.info("setting learnrates")
 
@@ -99,8 +88,8 @@ optimizer = optim.RMSprop(
         momentum=0.9,
         weight_decay=0.00004,
 )
-lr_scheduler = optim.lr_scheduler.MultiplicativeLR(optimizer, lambda epoch: config.lr_decay_rate ** (epoch+1))
 # fmt:on
+lr_scheduler = optim.lr_scheduler.MultiplicativeLR(optimizer, lambda epoch: config.lr_decay_rate ** (epoch + 1))
 scaler = cuda.amp.GradScaler()
 
 # counting parameters
@@ -115,16 +104,16 @@ SSPexels_train = SSPexels(file_list_path="/workspace/dataset_processing/train_se
 Pexels_train_loader = DataLoader(SSPexels_train, batch_size=config.train_batch_size, shuffle=True, drop_last=True, num_workers=config.num_workers)
 logging.info("datasets created")
 
-if config.warm_start:
-    g_step = config.warm_start_epoch * len(Pexels_train_loader)
-    for epoch in range(config.warm_start_epoch):
+if warm_epoch > 0:
+    g_step = warm_epoch * len(Pexels_train_loader)
+    for epoch in range(warm_epoch + 1):
         lr_scheduler.step()
 else:
     g_step = 0
 
 
 logging.info("start training")
-for epoch in range(config.warm_start_epoch, config.epochs):
+for epoch in range(warm_epoch + 1, 100):
     for i, data in enumerate(Pexels_train_loader):
         logging.info(f"batch loaded: step {i}")
 
@@ -133,16 +122,19 @@ for epoch in range(config.warm_start_epoch, config.epochs):
         with cuda.amp.autocast():
             losses = ia.calc_loss(data)
 
-        writer.add_scalar("loss/train/balanced/ranking", loss.item(), g_step)
+        for k, loss in losses.items():
+            writer.add_scalar(f"loss/train/balanced/{k}", loss.item(), g_step)
 
-        logging.info(f"Epoch: {epoch + 1}/{config.epochs} | Step: {i + 1}/{len(Pexels_train_loader)} | Training loss: {loss.item()}")
+        loss = sum([v for _, v in losses.items()])
+
+        logging.info(f"Epoch: {epoch} | Step: {i + 1}/{len(Pexels_train_loader)} | Training loss: {loss.item()}")
 
         # optimizing
         scaler.scale(loss).backward()
         scaler.step(optimizer)
         scaler.update()
 
-        writer.add_scalar("progress/epoch", epoch + 1, g_step)
+        writer.add_scalar("progress/epoch", epoch, g_step)
         writer.add_scalar("progress/step", i + 1, g_step)
         writer.add_scalar("hparams/lr/features", optimizer.param_groups[0]["lr"], g_step)
         writer.add_scalar("hparams/lr/classifier", optimizer.param_groups[1]["lr"], g_step)
@@ -169,7 +161,7 @@ for epoch in range(config.warm_start_epoch, config.epochs):
 
     logging.info("saving!")
     Path(config.ckpt_path).mkdir(parents=True, exist_ok=True)
-    torch.save(ia.state_dict(), str(Path(config.ckpt_path) / f"epoch-{epoch + 1}.pth"))
+    torch.save(ia.state_dict(), str(Path(config.ckpt_path) / f"epoch-{epoch}.pth"))
 
 logging.info("Training complete!")
 writer.close()
