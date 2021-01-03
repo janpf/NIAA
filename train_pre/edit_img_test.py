7d514361f60d33f68a6b307754f1e38c84622503
janpf <janpf@users.noreply.github.com>
2021-01-03 18:11:11 +0100

    testing everything

--- train_pre/edit_img_test.py
+++ train_pre/edit_img_test.py
@@ -2,21 +2,16 @@ from pathlib import Path
 import sys
 
 sys.path[0] = "."
-from train_pre.preprocess_images import ImageEditor
+from train_pre import preprocess_images
 
 in_f = Path("/home/janpf/projects/NIAA/test_imgs/test.jpg")
 out_f: Path = in_f.parent / "out"
 
 out_f.mkdir(parents=True, exist_ok=True)
 
-ed = ImageEditor()
+ed = preprocess_images.ImageEditor()
 
-dists = [
-    ("brightness", 1),
-    ("brightness", 3),
-    ("highlights", 3),
-    ("temperature", -4),
-]
+dists = preprocess_images.mapping["all_changes"]
 
 res = ed.distort_list_image(path=str(in_f), distortion_intens_tuple_list=dists)
 
